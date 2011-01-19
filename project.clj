c984e3ea8fa54bbf2f88a08fbebbe65ced23239b
Ilmari Vacklin <ilmari.vacklin@cs.helsinki.fi>
2011-01-20 01:05:19 +0200

    Don't depend on javax.mail

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject returns "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [javax.mail/mail "1.4.4"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[midje "1.0.0-COLLECTORS-EDITION"]
                      [swank-clojure "1.2.1"]]
   :repositories {"java.net" "http://download.java.net/maven/2"})
