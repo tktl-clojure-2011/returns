ed16086a7871ae0b4f723682851b7d4b80434fe4
Ilmari Vacklin <ilmari.vacklin@cs.helsinki.fi>
2011-01-20 01:05:45 +0200

    Initial user code functions

--- src/returns/core.clj
+++ src/returns/core.clj
@@ -6,8 +6,9 @@
 
 (defn protection-domain [permissions]
   (java.security.ProtectionDomain.
-   (java.security.CodeSource. nil
-                              (cast java.security.cert.Certificate nil))
+   (java.security.CodeSource.
+    nil
+    (cast java.security.cert.Certificate nil))
    permissions))
 
 (defn access-control-context [domain]
@@ -20,3 +21,16 @@
    (access-control-context
     (protection-domain
      (suitable-permissions)))))
+
+(defn make-user-tests [user code tests]
+  (map (fn [t] `(do (ns ~(gensym user) (:use clojure.test)) ~@code ~t)) tests))
+
+(defn read-user-file [filename]
+  (read-string (slurp filename)))
+
+(defn sanitize-leading-ns [user code]
+  (let [[head & body :as n] (first code)
+	m (vec n)]
+    (if (= head 'ns)
+      (cons (list* (assoc m 1 (gensym user))) (rest code))
+      code)))
